488e5ec93c736f17aaa59d86544029be3e53a4bf
aneteskrinda <anete.skrinda@gmail.com>
2023-02-27 00:08:38 +0200

    My changes

--- Python program/Hello_everyone.py
+++ Python program/Hello_everyone.py
@@ -10,8 +10,5 @@ print ("Hello, this is Marta!")
 print ("Hello, this is Inga")
 print("Hello, this is Ieva P")
 print("Hello, this is Indre")
-<<<<<<< HEAD
 print("Hello, this is Anete S")
-=======
 print("Hello, this is Monika Ras")
->>>>>>> ce67cbfa631f898c47d789a4be2bd2fff03e414c
