418cd50fa503f73b47ecd72c5f3532dafcf125ce
aneteskrinda <anete.skrinda@gmail.com>
2023-02-27 00:05:57 +0200

    My first commit

---
+++ Python program/Hello_everyone.py
@@ -0,0 +1,17 @@
+print ("Hello, this is Olga.")
+print ("Hello, this is Tatjana.")
+print("Hello, this is Inese")
+print ("Hello, this is Greta.")
+print ("Hello, this is Vlada!")
+print("Hi, this is Kristiana!")
+print ("Hello, this is Ieva")
+print("Hello, this is LauraZ")
+print ("Hello, this is Marta!")
+print ("Hello, this is Inga")
+print("Hello, this is Ieva P")
+print("Hello, this is Indre")
+<<<<<<< HEAD
+print("Hello, this is Anete S")
+=======
+print("Hello, this is Monika Ras")
+>>>>>>> ce67cbfa631f898c47d789a4be2bd2fff03e414c
